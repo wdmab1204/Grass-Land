229a6f2b475360c2257aa4a1c51495549800d854
wdmab1204 <wdmab1204@naver.com>
2023-01-27 16:22:51 +0900

    Simple한 몬스터 ai구현
    1. attack range tile을 화면에 표시
    2. 공격범위안에 플레이어가 들어가면 데미지를 줌

--- Assets/Scripts/TurnSystem/MonsterActor.cs
+++ Assets/Scripts/TurnSystem/MonsterActor.cs
@@ -7,6 +7,8 @@ using System.Collections.Generic;
 [DisallowMultipleComponent]
 public class MonsterActor : MonoBehaviour, ITurnActor
 {
+    enum BehaviourState { IDLE,FOLLOW,ATTACK };
+
     public GameObject ActorObject { get; set; }
     public ActorState ActorState { get; set; }
 
@@ -19,12 +21,14 @@ public class MonsterActor : MonoBehaviour, ITurnActor
     private Vector2Int[] scanRangeCoords;
     private readonly string attackRangeString =
         "[-1,1][0,1][1,1]" +
-        "[-1,0]     [0,1]" +
-        "[-1,-1][-1,0][-1,1]";
-    [SerializeField] private bool isFollowing = false;
+        "[-1,0]     [1,0]" +
+        "[-1,-1][0,-1][1,-1]";
+    private Vector2Int[] attackRangeCoords;
+    [SerializeField] private BehaviourState behaviour = BehaviourState.IDLE;
     private Navigation navigation;
     private Vector3Int LocalPosition { get => TilemapReader.ChangeWorldToLocalPosition(this.transform.position); }
     [SerializeField] private VisibilityTile scanRagneTilePrefab;
+    [SerializeField] private VisibilityTile attackRangeTilePrefab;
     [SerializeField] private TilemapReader TilemapReader;
     [SerializeField] private EntityManager EntityManager;
     [SerializeField] private TileGroup TileGroup;
@@ -36,30 +40,42 @@ public class MonsterActor : MonoBehaviour, ITurnActor
     public IEnumerator ActionCoroutine()
     {
         ActorState = ActorState.Start;
-        
-        if (isFollowing)
+
+        //플레이어가 시야범위안에 들어왔는지 체크
+        foreach (var coord in scanRangeCoords)
+        {
+            if (EntityManager.TryGetEntityOnTile<PlayerEntity>((Vector3Int)coord + LocalPosition, out Entity target))
+            {
+                behaviour = BehaviourState.FOLLOW;
+                this.target = target;
+                TileGroup.Hide();
+                break;
+            }
+        }
+
+        foreach(var coord in attackRangeCoords)
+        {
+            if (this.target == null) break;
+
+            if (EntityManager.TryGetEntityOnTile<PlayerEntity>((Vector3Int)coord + LocalPosition, out Entity target))
+            {
+                behaviour = BehaviourState.ATTACK;
+                this.target = target;
+                break;
+            }
+        }
+
+        if (behaviour == BehaviourState.FOLLOW)
         {
             //시야범위안에 있다면 쫒아감.
             navigation.SetDestination(target.transform.position);
             yield return navigation.GoDestination((TileNode)LocalPosition, end: navigation.Destination, this.transform, 1);
         }
-
-        else
+        else if(behaviour == BehaviourState.ATTACK)
         {
-            //플레이어가 시야범위안에 들어왔는지 체크
-            foreach (var coord in scanRangeCoords)
-            {
-                if (EntityManager.TryGetEntityOnTile<PlayerEntity>((Vector3Int)coord + LocalPosition, out Entity target))
-                {
-                    isFollowing = true;
-                    this.target = target;
-                    TileGroup.Hide();
-                    navigation.SetDestination(target.transform.position);
-                    yield return navigation.GoDestination((TileNode)LocalPosition, end: navigation.Destination, this.transform, 1);
-
-                }
-            }
+            target.TakeDamage(100);
         }
+
         ActorState = ActorState.End;
     }
 
@@ -69,11 +85,13 @@ public class MonsterActor : MonoBehaviour, ITurnActor
         this.transform.position = TilemapReader.RepositioningTheWorld(this.transform.position);
         ActorObject = this.gameObject;
         scanRangeCoords = CardNameSpace.Base.CoordConverter.ConvertToCoords(scanRangeString);
+        attackRangeCoords = CardNameSpace.Base.CoordConverter.ConvertToCoords(attackRangeString);
     }
 
     private void Start()
     {
         TileGroup.CreateClones(scanRagneTilePrefab, scanRangeCoords, LocalPosition);
+        TileGroup.CreateClones(attackRangeTilePrefab, attackRangeCoords, LocalPosition);
     }
 }
 
