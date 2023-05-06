a5984df211ee4d66344d18fe34fb56b47be0bf4f
wdmab1204 <wdmab1204@naver.com>
2023-05-06 17:10:41 +0900

    몬스터 주의 경계 상태 애니메이션 구현

---
+++ Assets/Scripts/BubbleAnimationController.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BubbleAnimationController : MonoBehaviour
+{
+    SpriteRenderer bubbleSprite;
+    Animator bubbleAnim;
+
+    public float radius = 5f;
+
+    void Awake()
+    {
+        bubbleSprite = transform.GetChild(0).GetComponent<SpriteRenderer>();
+        bubbleAnim = transform.GetChild(0).GetComponent<Animator>();
+    }
+
+    private void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        time = 0;
+    }
+
+    float time = 0f;
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        time += Time.deltaTime;
+        bubbleAnim.SetFloat("Time", time);
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        time = -1;
+        bubbleAnim.SetFloat("Time", time);
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, radius);
+    }
+
+    public void Flip()
+    {
+
+    }
+}
