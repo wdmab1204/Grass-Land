11dffbc12ff6ac7457b52888d105627c60614b74
wdmab1204 <wdmab1204@naver.com>
2023-05-22 22:54:41 +0900

    dust파티클 생성 및 구현

--- Assets/Scripts/Entity/HitStop.cs
+++ Assets/Scripts/Entity/HitStop.cs
@@ -10,7 +10,11 @@ public class HitStop : MonoBehaviour
     Rigidbody2D rb;
     float slowdownFactor = 0.95f; // 감속 계수
 
+    [Space()]
     public SoundPlayer player;
+
+    [Space()]
+    public ParticleSystem dust;
 
     private void Awake()
     {
@@ -44,18 +48,19 @@ public class HitStop : MonoBehaviour
         }
 
         rb.AddForce(force);
+        var mainModule = dust.main;
+        mainModule.duration = 0.002f * force.magnitude;
+        dust.Play();
         shakeTween.Kill();
     }
-
-
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         //Debug.Log(collision.name);
         if (collision.CompareTag("Skill"))
         {
-            Debug.Log("Hit stop");
-            Vector2 force = (this.transform.position - collision.transform.parent.position).normalized * 100f;
+            //if fource is 300, dust duration is 0.3, else if 100, duration is 0.1
+            Vector2 force = (this.transform.position - collision.transform.parent.position).normalized * 300f;
 
             ApplyHitStop(duration: 0.3f, timeScale: 0.3f, count: 1, force);
         }
